e67dbf0c94ae5ad3662980092a5c473de1044dc0
Temitope Joseph <126495757+Tjoseph-O@users.noreply.github.com>
2023-08-06 23:58:32 +0100

    last ones

---
+++ src/chapter_two/Print.java
@@ -0,0 +1,7 @@
+package chapter_two;
+
+public class Print {
+    public static void main(String[] args) {
+        System.out.printf("%s%n%s%n%s%n%s%n","  *", " ***","*****", " ***", "  *");
+    }
+}
--- src/chapter_two/ShapesAsterisks.java
+++ src/chapter_two/ShapesAsterisks.java
@@ -2,6 +2,17 @@ package chapter_two;
 
 public class ShapesAsterisks {
     public static void main(String[] args) {
+        for (int j = 0; j < 4; j++) {
+            System.out.print("**");
+        }
+        for (int i = 0; i < 7; i++) {
+            System.out.println("*");
+        }
+        for (int j = 0; j < 4; j++) {
+            System.out.print("**");
+        }
+
+
 
     }
       
